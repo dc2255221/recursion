b3e50c17c78ef3d87d5061eca6ed2f03239d93be
Don Chen <donchen@Dons-MacBook-Pro.local>
2019-12-01 16:21:03 -0800

    Completed

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,34 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof(obj) === 'number' || typeof(obj) === 'boolean') {
+    return obj.toString();
+  }
+
+  if (obj === null) {
+    return 'null';
+  }
+
+  if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)) {
+    let result = [];
+    _.each(obj, function(item) {
+      result.push(stringifyJSON(item))
+    })
+    return '[' + result.join(',') + ']';
+  }
+
+  if (typeof(obj) === 'object') {
+    let keyValuePairs = [];
+    for (let key in obj){
+      if (typeof(obj[key]) === 'function' || typeof(obj[key]) === 'undefined'){
+        continue;
+      } keyValuePair = '"' + key + '":' + stringifyJSON(obj[key]);
+      keyValuePairs.push(keyValuePair);
+    }
+    return '{' + keyValuePairs.join(',') + '}';
+  }
 };
